744f811cc2b9d9a4f94859c183cdd4905b9a6956
Ramiz A <ramiz.demiurge@gmail.com>
2018-03-25 16:32:18 +0400

    ничего важноо

--- functions/djaler_utils.py
+++ functions/djaler_utils.py
@@ -6,6 +6,7 @@ from telegram import Bot, User
 
 @supycache.supycache(cache_key='admin_ids_{1}', max_age=10 * 60)
 def get_admin_ids(bot: Bot, chat_id):
+    print("Caching admins list...")
     return [admin.user.id for admin in bot.get_chat_administrators(chat_id)]
 
 
