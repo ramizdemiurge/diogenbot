595b9322a19b913fb19d48fd3f85b7108cac538c
Ramiz A <ramiz.demiurge@gmail.com>
2018-04-06 01:28:15 +0400

    ~ lists

--- model/lists.py
+++ model/lists.py
@@ -4,4 +4,6 @@ banned_words = ["@", "http", ".com", "казино", "выигр"]
 thank_words = ["пасиб", "посиб", "спс", "благодар", "сэнкс", "сенкс", "дякую", "thanx", "thank"]
 interest_words = os.environ.get("INTEREST", "рамиз").split(" ")
 
-
+super_admin_ids = (76114490, 251521119)
+info_kanal = "diobage"
+log_chat_second = -268646012
